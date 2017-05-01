f7aa873bba19152b173dc45a1a92065e297412e2
Mike He <heminhao@hotmail.com>
2017-05-01 17:01:19 +0800

    add myobj api

---
+++ api/controllers/MyobjController.js
@@ -0,0 +1,11 @@
+/**
+ * MyobjController
+ *
+ * @description :: Server-side logic for managing myobjs
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
